7a2b2de588d87f7fbb07d775049a46f2382900e3
heyuan <heyuan@carben.me>
2018-05-30 01:02:10 +0800

    tinker集成，以及java转kotlin发补丁

---
+++ app/src/main/java/com/keven/kotlin/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.keven.kotlin
+
+import android.os.Bundle
+import android.os.Environment
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import com.keven.kotlin.R.id.textview_home
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller
+import com.tinkerpatch.sdk.TinkerPatch
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+
+        TinkerPatch.with().fetchPatchUpdate(true)
+        //
+        //        TextView textView = findViewById(R.id.textview);
+
+//                textview.setText("这是没有bug版本");
+        val textView = findViewById<TextView>(R.id.textview_home)
+        textView.text = "kotlin 补丁成功"
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+}
